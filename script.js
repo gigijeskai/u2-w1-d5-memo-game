7ac3aa5ee55b74e421cd6bd7e8605f26ebd54ea4
Luigi Indaco <luigindaco@gmail.com>
2022-10-02 15:58:18 +0200

    simple commit

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
  let carta1;
  let carta2;
-let tutteLeCarte = document.querySelectorAll('card')
+let tutteLeCarte = document.getElementsByClassName('card');
 
 
 function flip(giraCarta){
  giraCarta.target.classList.add('flip')
-//  ^^per ogni carta che seleziono applica uno sfondo blu
+
 if (carta1 === undefined){
     carta1 = giraCarta.target
+   
     } else {
         carta2 = giraCarta.target
-    }
+    
     for (let i = 0; i < tutteLeCarte.length;i++){
 tutteLeCarte[i].removeAttribute('onclick')
-    }    
+}    
+
+ let carteUguali = confrontaCarte()
+if (carteUguali === true){
+carta1.classList.replace('flip','match')
+carta2.classList.replace('flip','match')
+} else {
+    carta1.classList.remove('flip')
+    carta2.classList.remove('flip')
+}
+}
+}
+function  confrontaCarte(){
+    let carteselezionate = [carta1,carta2];
+    return carteselezionate
 }
 
 
